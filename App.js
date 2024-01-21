abee4dbe539b52d51257f9f0a32fad322406c494
Steven Egnaczyk <stevenegnaczyk@gmail.com>
2024-01-21 18:03:40 -0500

    :D

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.bigBlueText}>Chalkbets</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'gray', // Set the background color to gray
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bigBlueText: {
+        fontSize: 40,
+        color: 'lightgreen',
+        fontWeight: 'bold',
+    },
 });
