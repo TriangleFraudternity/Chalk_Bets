ff16bb9b75cf8d64a71ae5113150f6d3d8c4c0c5
zzteddy <edted22@gmail.com>
2024-02-05 00:07:33 -0500

    validation

--- pages/ForgorPasswordScreen.js
+++ pages/ForgorPasswordScreen.js
@@ -3,32 +3,43 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import {getAuth, sendPasswordResetEmail} from "firebase/auth";
+import {validateEmail} from '../helpers/validationHelper.js';
+
 
 import {auth} from '../config/firebase.js';
 
 const ForgorPasswordScreen = ({ navigation }) => {
   const [resetEmailStatus, setResetEmailStatus] = useState(0);
   const [email, setEmail] = useState('');
-  const resetPasswordStatusMessages = ['nuffin yet','Email Sent','Email not Found']
+  const resetPasswordStatusMessages = ['nuffin yet','Email Sent','Email not Found'];
+  const [emailValidationMessage, setEmailValidationMessage] = useState('');
+
 
   const auth = getAuth();
 
+  
   sendPasswordResetEmailFunction = () => {
-    console.log('Reset Password');
-    sendPasswordResetEmail(auth, email)
-    .then(() => {
-        // Password reset email sent!
-        // ..
-        setResetEmailStatus(1);
-    })
-    .catch((error) => {
-        console.log("wtfbruh");
-        setResetEmailStatus(2);
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        
-    });
+      if (validateEmail(email)) {
+      console.log('Reset Password');
+      sendPasswordResetEmail(auth, email)
+      .then(() => {
+          // Password reset email sent!
+          // ..
+          setResetEmailStatus(1);
+      })
+      .catch((error) => {
+          console.log("wtfbruh");
+          setResetEmailStatus(2);
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          
+      });
+    }
+    else{
+      setEmailValidationMessage('Not a valid email');
+    }
   };
+  
 
   return (
     <View style={styles.container}>
@@ -40,6 +51,7 @@ const ForgorPasswordScreen = ({ navigation }) => {
         placeholder="Enter your email"
         keyboardType="email-address"
       />
+      <Text style={styles.label}>{emailValidationMessage}</Text>
       <Button title="Send Recovery Email" onPress={sendPasswordResetEmailFunction} />
       <Text style={styles.outPut}>{resetPasswordStatusMessages[resetEmailStatus]}</Text>
 
